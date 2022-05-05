d463a5e87eb5b162c2990913416537bdda92e83b
vinodh1988 <k.c.vinodh@hotmail.com>
2022-05-05 16:33:17 +0530

    first-commit

---
+++ config/config.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import boto3
+
+import os
+
+app=Flask(__name__,static_url_path="")
+
+
+client = boto3.client('s3', 
+         region_name='ap-south-1',
+         aws_access_key_id=os.getenv('ACCESS_KEY'),
+         aws_secret_access_key=os.getenv('SECRET_KEY')
+      )
